04090703c5537108af5cf625ef4a257e0ec554c9
Matvey <matveyprys@gmail.com>
2026-02-07 13:18:14 +0200

    need to watch video

---
+++ src/app.js
@@ -0,0 +1,67 @@
+// // import Chart from 'chart.js';
+// import Chart from 'chart.js/auto';
+// const graphRef = document.querySelector(".graf")
+// const data = {
+
+// labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+
+
+// datasets: [{
+
+// label: 'Temperature',
+
+// data: [13, 15, 17, 18, 20, 12, 30],
+
+// fill: false,
+
+// // borderColor: 'rgb(75, 192, 192)',
+//  backgroundColor: [
+//       'rgb(255, 99, 132)',
+//       'rgb(54, 162, 235)',
+//       'rgb(255, 205, 86)',
+//       'rgb(0,255,0)',
+//       'rgb(255,100,100)'
+//     ],
+
+// tension: 0.1
+
+// }]
+
+// };
+
+// // const config = {
+
+// // type: 'line',
+
+// // data: data,
+
+// // options: {}
+
+// // };
+// // const config = {
+// //   type: 'bar',
+// //   data: data,
+// //   options: {
+// //     scales: {
+// //       y: {
+// //         beginAtZero: true
+// //       }
+// //     }
+// //   },
+// // };
+// // const config = {
+// //   type: 'doughnut',
+// //   data: data,
+// // };
+// const config = {
+//   type: 'polarArea',
+//   data: data,
+//   options: {}
+// };
+
+
+// new Chart(graphRef, config)
+
+// --------------------
+
+const btnRef = document.querySelector(".btn")
